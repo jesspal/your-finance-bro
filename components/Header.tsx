f805be86d0eec09cd692d923617fd38d924c975a
jesspal <jesspdev@gmail.com>
2025-11-13 14:30:32 -0500

    started header

---
+++ components/Header.tsx
@@ -0,0 +1,13 @@
+import { Link } from "lucide-react"
+
+const Header = () => {
+  return (
+    <header className="sticky top-0 header">
+        <div className="container header-wrapper">
+            <Link href="/"></Link>
+        </div>
+    </header>
+  )
+}
+
+export default Header
