a31a63ae4f94baa3886bc578edff5b1095221c24
jesspal <jesspdev@gmail.com>
2025-11-14 13:00:22 -0500

    updated page to root directory, created header

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -4,6 +4,7 @@ const Layout = ({ children }: { children: React.ReactNode}) => {
   return (
     <main className="min-h-screen text-gray-400">
         <Header/>
+        
         <div className="container py-10">
             { children }
         </div>
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,10 +1,17 @@
-import { Link } from "lucide-react"
+import Link from "next/link"
+import Image from "next/image"
 
 const Header = () => {
   return (
     <header className="sticky top-0 header">
         <div className="container header-wrapper">
-            <Link href="/"></Link>
+            <Link href="/">
+                <Image src="/assets/icons/logo.svg" alt="Signalist logo" width={140} height={32} className="h-8 w-auto cursor-pointer"></Image>
+            </Link>
+            <nav className="hidden sm:block">
+                {/* NavItems */}
+            </nav>
+        {/* UserDropdown */}
         </div>
     </header>
   )
