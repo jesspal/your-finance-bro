83dc77965ed8a7f8957a4b1535e3d0aa2add6d28
jesspal <jesspdev@gmail.com>
2026-01-21 14:37:34 -0500

    implement authentication logic

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 import NavItems from "./NavItems"
 import UserDropdown from "./UserDropdown"
 
-const Header = () => {
+const Header = ({ user }: { user: User }) => {
   return (
     <header className="sticky top-0 header">
         <div className="container header-wrapper">
@@ -13,8 +13,8 @@ const Header = () => {
             <nav className="hidden sm:block">
                 <NavItems />
             </nav>
-            
-            <UserDropdown />
+
+            <UserDropdown user={user}/>
         </div>
     </header>
   )
